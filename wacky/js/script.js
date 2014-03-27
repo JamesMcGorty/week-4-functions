09bc23ed473640b530cd9141ee248dc6ae1f994e
James McGorty <jbmcgorty@gmail.com>
2014-03-27 09:58:11 -0400

    wacky js

--- wacky/js/script.js
+++ wacky/js/script.js
@@ -8,3 +8,6 @@
 var money = prompt("How much would you like to withdaw ?", 100);
 var bill = prompt("What is the largest bill you would like?", 20);
 var receipt = prompt("Would you like a reciept?", "No");
+
+// Annoymous function to calcualte the number of bills 
+var calcNumBills = function() { return Math.floor(money / bill); };
